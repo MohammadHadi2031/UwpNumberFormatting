14fbc3b4c6501880e75cbb8daceb9fe329b3bba7
MohammadHadi Attarieh <Mhadi.attarieh@gmail.com>
2021-09-10 04:00:00 +0430

    Add NumeralSystemTranslator

---
+++ NumeralSystemTests.Uwp/Given_NumeralSystemTranslator.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Windows.Globalization.NumberFormatting
+{
+    [TestClass]
+    public class Given_NumeralSystemTranslator
+    {
+        [DataTestMethod]
+        [DataRow("1,234,567,890.12 % ‰", "Arab", "١٬٢٣٤٬٥٦٧٬٨٩٠٫١٢ % ‰")]
+        [DataRow("1,234,567,890.12%", "Arab", "١٬٢٣٤٬٥٦٧٬٨٩٠٫١٢%")]
+        [DataRow("1,234,567,890.12 % ‰", "ArabExt", "۱٬۲۳۴٬۵۶۷٬۸۹۰٫۱۲ % ‰")]
+        public void When_NumeralSystemIsArab(string value, string numeralSystem, string expected)
+        {
+            NumeralSystemTranslator numeralSystemTranslator = new NumeralSystemTranslator();
+            numeralSystemTranslator.NumeralSystem = numeralSystem;
+            var translated = numeralSystemTranslator.TranslateNumerals(value);
+            Assert.AreEqual(expected, translated);
+        }
+    }
+}
