e7b55d9657abfd32f04d67ea4ea62badd58394e5
MohammadHadi Attarieh <mohammadhadi2031@gmail.com>
2022-03-19 00:00:35 +0330

    add project to test ThreadStatic

---
+++ ThreadStaticTestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+class Program
+{
+    //[ThreadStatic] static StringBuilder stringBuilder;
+    static ThreadLocal<StringBuilder> stringBuilderWrapper = new ThreadLocal<StringBuilder>(() => new StringBuilder());
+
+    public static void Main(string[] args)
+    {
+        Task.Run(Print);
+        Print();
+    }
+
+    private static void Print()
+    {
+        var stringBuilder = stringBuilderWrapper.Value!;
+        
+        stringBuilder.Append("hello *" + Thread.CurrentThread.ManagedThreadId);
+        Console.WriteLine(stringBuilder.ToString());
+    }
+}
